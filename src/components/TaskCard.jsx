7c84a20707413a80be94d870bd7363b7ebb86cae
antony-chi <antony.cp9@gmail.com>
2023-06-05 13:08:38 -0600

    accediendo a la varible x desde TaskCard ejemplo useContext()

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,14 +1,21 @@
-import {TaskContext} from "../context/TaskContext"
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
 
 export const TaskCard = ({ task, deleteTask }) => {
+  const valor = useContext(TaskContext);
+  console.log(valor)
 
   return (
-    <TaskContext>
-      <div>
+    <div>
       <h1>{task.title}</h1>
       <h2>{task.description}</h2>
-      <button onClick={() => {deleteTask(task.id)}} >Eliminar</button>
+      <button
+        onClick={() => {
+          deleteTask(task.id);
+        }}
+      >
+        Eliminar
+      </button>
     </div>
-    </TaskContext>
   );
 };
--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,8 +1,11 @@
-export const TaskContext = (props) => {
+import { createContext } from "react";
+
+export const TaskContext = createContext(); //nombre del contexto
+export const TaskContextProvider = (props) => {
+  //nombre del componente que engloba a todo
+  let x = 20
   return (
-    <>
-        <h1>Componente TaskContext</h1>
-        {props.children}
-    </>
-  )
-}
+    //se crea el componente
+    <TaskContext.Provider value={x}>{props.children}</TaskContext.Provider>
+  );
+};
